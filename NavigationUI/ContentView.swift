1a928221f147293f16a654c1953a05b582146adb
Kapil_bhattarai <kapilbhattarai5@gmail.com>
2021-02-09 17:22:31 +0545

    initial commit

---
+++ NavigationUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NavigationUI
+//
+//  Created by Kapil Bhattarai on 09/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NavigationUI/NavigationUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationUIApp.swift
+//  NavigationUI
+//
+//  Created by Kapil Bhattarai on 09/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct NavigationUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
