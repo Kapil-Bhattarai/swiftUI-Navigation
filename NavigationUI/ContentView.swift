b4bc4b4c37e5e0ae64b0e9d578c9307002655bcb
Kapil_bhattarai <kapilbhattarai5@gmail.com>
2021-02-10 10:39:18 +0545

    add: navigationUI

--- NavigationUI/ContentView.swift
+++ NavigationUI/ContentView.swift
@@ -9,13 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Image(systemName: "house")
+                    .resizable()
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding()
+                Text("First Screen")
+                    .font(.system(size: 20, weight: .bold, design: .default))
+                    .padding()
+                HStack{
+                    NavigationLink(
+                        destination: Text("Hello World"),
+                        label: {
+                            ContinueButton(btnTitle: "First Screen")
+                        })
+                    NavigationLink(
+                        destination: Image(systemName: "bell")
+                            .resizable()
+                            .frame(width: 200, height: 200, alignment: .center)
+                            .padding(),
+                        label: {
+                            ContinueButton(btnTitle: "Second Screen")
+                        })
+                }
+            }
+            .navigationTitle("Home")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ContinueButton: View {
+    var btnTitle: String
+    var body: some View {
+        Text(btnTitle)
+            .frame(width: 200, height: 50, alignment: .center)
+            .background(Color.pink)
+            .foregroundColor(.white)
+            .cornerRadius(8)
     }
 }
